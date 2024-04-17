a22321e3214e7ab40e49240d7e6c3c638cc8545f
Carls <57661996+BugSharer@users.noreply.github.com>
2024-04-17 17:18:04 +0800

    Initial commit
    Edge浏览器套壳程序示例，用于使能配置项：始终在新选项卡中打开收藏夹

---
+++ Edge Shell.bat
@@ -0,0 +1,3 @@
+@echo off
+start "Microsoft Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge_Raw.exe" --enable-features=msFavoritesOpenInNewTabSetting %1 %2 %3 %4 %5 %6 %7 %8 %9
+exit
